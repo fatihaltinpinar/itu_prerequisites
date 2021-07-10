318397fc6f2cb57d1215e028ed835a09eae6b531
Fatih ALTINPINAR <fatihaltinpinar@gmail.com>
2021-07-10 22:46:18 +0300

    Implement color updating

--- onsart/javascript/onsart.js
+++ onsart/javascript/onsart.js
@@ -1,6 +1,48 @@
 let courseDict = {}
-let nodeList = []
-let edgeList = []
+ // create an array with nodes
+var nodes = new vis.DataSet([]);
+// create an array with edges
+var edges = new vis.DataSet([]);
+
+
+
+color_completed = {
+      border: '#ff9900',
+      background: '#ffbd38',
+      highlight: {
+        border: '#ff9900',
+        background: '#ffbd38'
+      },
+      hover: {
+        border: '#ff9900',
+        background: '#ffbd38'
+      }
+    }
+color_selectable = {
+    border: '#d4bd8e',
+    background: '#f0d9aa',
+    highlight: {
+        border: '#d4bd8e',
+        background: '#f0d9aa'
+    },
+    hover: {
+        border: '#d4bd8e',
+        background: '#f0d9aa'
+    },
+}
+color_nonselectable = {
+    border: '#9c9a94',
+    background: '#bab9b6',
+    highlight: {
+        border: '#9c9a94',
+        background: '#bab9b6'
+    },
+    hover: {
+        border: '#9c9a94',
+        background: '#bab9b6'
+    },
+}
+
 
 class Course{
     constructor(id, semester, preqs){
@@ -10,27 +52,38 @@ class Course{
         this.isSelectable = false;
         this.isCompleted = false;
         this.preqTo = [];
-        nodeList.push({id:this.id, label:this.id});
+        nodes.update({id:this.id, level: this.semester, label:this.id, title:"course name goes here"});
+
+        var table = document.getElementById("semester"+this.semester);
+        var row = table.insertRow(1);
+        row.id = this.id;
+        row.onclick = function() {courseDict[this.id].toggle();};
+        row.innerHTML = "<td>" + this.id + "</td><td>" + this.id + "</td><td>"+ this.semester + "</td>"
 
         for(var i = 0; i<preqs.length; i++){
-            console.log("burdayiz preq = "+ preqs);
             if (preqs[i] in courseDict){
-                console.log("hacim burdayiz preq = " + preqs[i]);
                 this.preqs.push(courseDict[preqs[i]]);
-                edgeList.push({from: preqs[i], to: this.id});
+                edges.update({from: preqs[i], to: this.id, arrows: 'to'});
                 courseDict[preqs[i]].preqTo.push(this);
             }
         }
-        if (semester === 0)
+        if (this.preqs.length === 0){
             this.isSelectable = true;
-    }
+            this.update();
+        }
+        if(this.isSelectable){
+            this.setColor(color_selectable);
+        } else {
+            this.setColor(color_nonselectable);
+        }
+       }
 
     select (){
         if (this.isSelectable){
             this.isCompleted = true;
+            this.setColor(color_completed);
             for (var i = 0; i<this.preqTo.length; i++){
                 this.preqTo[i].update();
-
             }
         }
     }
@@ -42,6 +95,8 @@ class Course{
                 this.preqTo[i].update();
             }
         }
+        this.setColor(color_selectable);
+        this.update();
     }
 
     update(){
@@ -54,13 +109,103 @@ class Course{
                 }
             }
         }
-        if (this.semester === 0){
+        if (this.preqs.length === 0){
             this.isSelectable = true;
         }
+        if(this.isSelectable){
+            this.setColor(color_selectable);
+        }else{
+            this.setColor(color_nonselectable);
+        }
+    }
+
+    setColor(color){
+        var node = nodes.get(this.id);
+        node.color = color;
+        nodes.update(node);
+        var row = document.getElementById(this.id);
+        row.classList.remove("selectable");
+        row.classList.remove("nonselectable");
+        row.classList.remove("completed");
+        if(color === color_selectable){
+            row.classList.add("selectable");
+        }
+        if(color === color_nonselectable){
+            row.classList.add("nonselectable");
+        }
+        if(color === color_completed){
+            row.classList.add("completed");
+        }
+    }
+
+    toggle(){
+        if (this.isCompleted)
+            this.deSelect();
+        else
+            this.select();
     }
 
 
 }
 
-courseDict["BLG1"] = new Course("BLG1", 0, []);
+courseDict["BLG1"] = new Course("BLG1", 1, []);
 courseDict["BLG2"] = new Course("BLG2", 2, ["BLG1"]);
+courseDict["BLG3"] = new Course("BLG3", 2, ["BLG1"]);
+courseDict["BLG4"] = new Course("BLG4", 3, ["BLG2"]);
+courseDict["BLG5"] = new Course("BLG5", 3, ["BLG3"]);
+courseDict["BLG6"] = new Course("BLG6", 4, ["BLG5", "BLG4"]);
+courseDict["BLG31"] = new Course("BLG31", 3, []);
+courseDict["BLG32"] = new Course("BLG32", 4, ["BLG31"])
+
+
+// create a network
+var container = document.getElementById('network');
+var data = {
+  nodes: nodes,
+  edges: edges
+};
+var options = {
+    interaction: {
+        hover:true,
+        tooltipDelay: 300
+    },
+    height: '400px',
+    edges: {
+        smooth: false,
+        labelHighlightBold: false,
+        hoverWidth: 0,
+    },
+    nodes: {
+        borderWidth: 4,
+        borderWidthSelected: 4,
+        size: 20,
+        shape: 'hexagon',
+        fixed: {
+            x: true,
+            y: true,
+        },
+        labelHighlightBold: false,
+
+    },
+    layout: {
+        randomSeed: 1,
+        improvedLayout: false,
+        hierarchical: {
+            enabled: true, //change to true to see the other graph
+            direction: 'LR',
+            nodeSpacing: 100,
+            sortMethod: 'directed',
+            shakeTowards: 'roots',
+    }
+  },
+  // configure: {},
+  "physics": {
+  "enabled": false,
+}
+};
+var network = new vis.Network(container, data, options);
+network.on('click', function(params){
+    if(params.nodes.length > 0){
+        courseDict[params.nodes[0]].toggle();
+    }
+})
